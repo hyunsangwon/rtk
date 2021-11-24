6094041a307b67756fec06cca2dab5aa659b133e
hyunsangwon <hyunsangwon93@gmail.com>
2021-11-24 15:30:14 +0900

    feat : search bar 추가

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import user from '../../images/user.png';
 import './Header.scss';
 
 const Header = () => {
+  const [term, setTerm] = useState('');
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(term);
+  };
   return (
     <div className="header">
-      <Link to="/">
-        <div className="logo">현상원의 Movie Time</div>
-      </Link>
+      <div className="logo">
+        <Link to="/">현상원의 Movie Time</Link>
+      </div>
+      <div className="search-bar">
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            value={term}
+            placeholder="Search Movies or Shows..."
+            onChange={(e) => setTerm(e.target.value)}
+          />
+          <button type="submit">
+            <i className="fa fa-search"></i>
+          </button>
+        </form>
+      </div>
       <div className="user-image">
         <img src={user} alt="user" />
       </div>
