aeb95c7527ebc54857971c4fd6386807db8b48cd
hyunsangwon <hyunsangwon93@gmail.com>
2021-11-24 10:55:45 +0900

    fix : api 호출 제거

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import MovieListing from '../MovieListing/MovieListing';
 import { useEffect } from 'react';
-import movieApi from '../../common/apis/MovieApi';
-import { APIKey } from '../../common/apis/MovieApiKey';
 import { useDispatch } from 'react-redux';
-import { addMovies } from '../../features/movies/movieSlice';
+import { fetchAsyncMovies } from '../../features/movies/movieSlice';
 
 const Home = () => {
-  const movieText = 'twilight';
   const dispatch = useDispatch();
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await movieApi
-        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
-        .catch((err) => {
-          console.log('Err : ' + err);
-        });
-      dispatch(addMovies(response.data));
-    };
-    fetchMovies();
-  }, []); //Empty array means to only run once on mount.
+    dispatch(fetchAsyncMovies());
+  }, [dispatch]); //Empty array means to only run once on mount.
 
   return (
     <div>
